fd25eeff2e48ec009e429b903303bf71f408bb2b
Jacques Gay <96320293+JacquesGay@users.noreply.github.com>
2023-03-28 10:44:59 -0400

    Update state_switching_correction.py

--- python/state_switching_correction.py
+++ python/state_switching_correction.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Apr 13 15:22:23 2022
-
 State Switching Correction
-
 @author: jgay15
 """
 
@@ -36,7 +34,7 @@ def sorting(filename):
             #outfile.writelines('\n')
     outfile.close()
 
-def index(filename):
+def index(filename, excited_state):
     k = 1
     with open(filename) as fn:
         lines = fn.read().splitlines()
@@ -47,41 +45,46 @@ def index(filename):
                 print(line + "  Index " + k, file=fn)
                 k = int(k)
                 k = k + 1
-            if k == 8:
+            if k == excited_state + 1:
                 k = 1
 
-file_loc = os.path.join ('lumiflavin','*.log')  #searches for all .log
+def project_file(excited_state):
+    max_state=excited_state + 1
+    file_loc = os.path.join ('log_files','*.log')  #searches for all .log
 #print(file_loc)
 
 
-project_batch=glob.glob(file_loc)  #put in order command 
+    project_batch=glob.glob(file_loc)  #put in order command 
 #print(project_batch)
 
-with open('projects.txt', 'w') as datafile: #opens file for writing
-    for f in project_batch:
-        project_id_1 = os.path.basename(f).split('.')[0]
-        project_id = project_id_1.split('_')[2]
-        with open(f, 'r') as outfile:
-            data = outfile.readlines()
+    with open('projects.txt', 'w') as datafile: #opens file for writing
+        for f in project_batch:
+            project_id_1 = os.path.basename(f).split('.')[0]
+            project_id = project_id_1.split('_')[2]
+            with open(f, 'r') as outfile:
+                data = outfile.readlines()
         
-        job = 1
-        for line in data:
-            for i in range (1,8):
-                if f"Excited State   {i}" in line:
-                    state_line = line
-                    factors = state_line.split()
-                    State_1 = factors[2]
-                    State = State_1.split(':')[0]
-                    Energy = factors[4]
-                    Oscillator = factors[8]
-                    oscstr = Oscillator.split('=')[1]
-                    datafile.write('Project Batch {:>2}  job {:2}   Oscillator Strength {:2}   {:2} eV  Excited State  {:2}\n'.format(project_id, job, oscstr, Energy, State))
-                if 'Normal termination of Gaussian' in line:
-                    job = job + 1
-datafile.close()
+            job = 1
+            for line in data:
+                for i in range(1, max_state):
+                    if f"Excited State   {i}" in line:
+                        state_line = line
+                        factors = state_line.split()
+                        State_1 = factors[2]
+                        State = State_1.split(':')[0]
+                        Energy = factors[4]
+                        Oscillator = factors[8]
+                        oscstr = Oscillator.split('=')[1]
+                        datafile.write('Project Batch {:>2}  job {:2}   Oscillator Strength {:2}   {:2} eV  Excited State  {:2}\n'.format(project_id, job, oscstr, Energy, State))
+                    if 'Normal termination of Gaussian' in line:
+                            job = job + 1
+    datafile.close()
 
+excited_state = int(input("Input number of excited states you wish to consider: \n"))
+
+project_file(excited_state)
 
 sorting('projects.txt')
 
-index('sorted.txt')
+index('sorted.txt', excited_state)
         
